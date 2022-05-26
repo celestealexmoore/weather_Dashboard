aec96a8a2e8fec2cbe88b495b3c2678828a956e1
Celeste Moore <celestealexmoore@gmail.com>
2022-05-25 19:10:30 -0500

    setting up html structure via js

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const body = document.getElementById('body');
+const title = document.createElement('div');
+//leftDiv
+const leftDiv = document.createElement('div');
+const searchDiv = document.createElement('div');
+const searchBar = document.createElement('i');
+const searchBarIcon = document.createElement('div');
+const appendList = document.createElement('div');
+//rightDiv top
+const rightDiv = document.createElement('div');
+const cityOverview = document.createElement('div');
+const cityName = document.createElement('div');
+const cityUl = document.createElement('ul');
+const cityLi = document.createElement('li');
+//rightDiv bottom
+const forecastDiv = document.createElement('div');
+const forecastUl = document.createElement('ul');
+const forecastLi = document.createElement('li');
