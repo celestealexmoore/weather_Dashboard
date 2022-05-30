e23aef7e6fcdc5211f57ca33fe6f5ff70774b626
Celeste Moore <celestealexmoore@gmail.com>
2022-05-30 14:26:59 -0500

    adjusting divs with bootstrap

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,20 @@ const requestURL =
 
 const body = document.getElementById("body");
 const title = document.createElement("div");
+const contentDivContainer = document.createElement("div");
+const contentDivRow = document.createElement("div");
 //leftDiv
-const leftDiv = document.createElement("div");
-const searchDiv = document.createElement("div");
-const searchBar = document.createElement("i");
-const searchBarIcon = document.createElement("div");
-const appendList = document.createElement("ul");
-const appendListItem = document.createElement("li");
+const leftDivContainer = document.createElement("div");
+const leftDivRow = document.createElement("div");
+const leftColTitle = document.createElement("div");
+
+// const appendList = document.createElement("ul");
+// const appendListItem = document.createElement("li");
+
 //rightDiv top
-const rightDiv = document.createElement("div");
+const rightDivContainer = document.createElement("div");
+const rightDivRow = document.createElement("div");
+
 const cityOverview = document.createElement("div");
 const cityName = document.createElement("div");
 const cityUl = document.createElement("ul");
@@ -34,37 +39,63 @@ function getAPI() {
       console.log(data);
     });
 
-  title.setAttribute("class", "row display-5");
+  title.setAttribute("class", "h1 row justify-content-center title");
   title.textContent = "Weather Dashboard";
-  // leftDiv
-  leftDiv.setAttribute("id", "leftDiv");
-  leftDiv.setAttribute("class", "leftDiv col-4");
+  contentDivContainer.setAttribute("class", "container-fluid");
+  contentDivRow.setAttribute("class", "row");
+  // leftDivContainer
+  leftDivContainer.setAttribute("id", "leftDivContainer");
+  leftDivContainer.setAttribute("class", "col-4");
+  leftDivRow.setAttribute("class", "row");
+  leftColTitle.setAttribute("class", "col h4");
+  leftColTitle.textContent = "Search for a City:";
+  // searchBar Structure
+  const inputGroup = document.createElement("div");
+  inputGroup.setAttribute("class", "input-group");
+  const formOutline = document.createElement("div");
+  formOutline.setAttribute("class", "form-outline");
+  const searchInput = document.createElement("input");
+  searchInput.setAttribute("type", "search");
+  searchInput.setAttribute("id", "form1");
+  searchInput.setAttribute("class", "form-control");
+  const searchLabel = document.createElement("label");
+  searchLabel.setAttribute("class", "form-label");
+  searchLabel.setAttribute("for", "form1");
+  //search button
+  const searchButton = document.createElement("div");
+  searchButton.setAttribute("id", "search-button");
+  searchButton.setAttribute("type", "button");
+  searchButton.setAttribute("class", "btn btn-primary");
+  const searchIcon = document.createElement("i");
+  searchIcon.setAttribute("class", "bi bi-search");
 
-  searchDiv.setAttribute("class", "h4");
-  searchDiv.textContent = "Search for a City:";
-  // searchBar
-  searchBar.setAttribute("class", "form-control form-control-sm col");
-  searchBar.setAttribute("type", "text");
-  searchBar.setAttribute("placeholder", "Enter a city name.");
-  //   icon
-  searchBarIcon.setAttribute("class", "bi bi-search searchBarIcon col");
   //append list
-  appendList.setAttribute("class", "list-group");
-  appendListItem.setAttribute(
-    "class",
-    "list-group-item list-group-item-action appendListItem"
-  );
+  //   appendList.setAttribute("class", "list-group");
+  //   appendListItem.setAttribute(
+  //     "class",
+  //     "list-group-item list-group-item-action appendListItem"
+  //   );
   // the line of code above should include an on hover, add class "active"
 
-  rightDiv.setAttribute("id", "rightDiv");
-  rightDiv.setAttribute("class", "col-8");
+  rightDivContainer.setAttribute("id", "rightDiv");
+  rightDivContainer.setAttribute("class", "col");
 
   body.appendChild(title);
-  body.appendChild(leftDiv);
-  leftDiv.appendChild(searchDiv);
-  searchDiv.appendChild(searchBar);
-  searchBar.appendChild(searchBarIcon);
-  body.appendChild(rightDiv);
+  body.appendChild(contentDivContainer);
+  contentDivContainer.appendChild(contentDivRow);
+  contentDivRow.appendChild(leftDivContainer);
+  leftDivContainer.appendChild(leftDivRow);
+  contentDivRow.appendChild(rightDivContainer);
+  rightDivContainer.appendChild(rightDivRow);
+  leftDivContainer.appendChild(leftColTitle);
+
+  leftDivContainer.appendChild(inputGroup);
+  inputGroup.appendChild(formOutline);
+  inputGroup.appendChild(formOutline);
+  formOutline.appendChild(searchInput);
+  formOutline.appendChild(searchLabel);
+  searchButton.appendChild(searchIcon);
+  inputGroup.appendChild(searchButton);
 }
 
 getAPI();
